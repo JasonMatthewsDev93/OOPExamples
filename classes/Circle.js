a4b6341856d2d2b0cb517246401d0b0a128542c1
Artexacast <jason_matthews09@hotmail.co.uk>
2023-06-04 19:26:34 +0100

    Example

--- classes/Circle.js
+++ classes/Circle.js
@@ -1,14 +1,6 @@
 const Shape = require('./shape');
 
 class Circle extends Shape {
-  constructor(radius) {
-    super();
-    this.radius = radius;
-  }
-
-  area() {
-    return Math.PI * Math.pow(this.radius, 2); // Overwritten area method in Circle class
-  }
 }
 
 module.exports = Circle;
--- index.js
+++ index.js
@@ -2,6 +2,3 @@ const Circle = require('./classes/Circle');
 const Rectangle = require('./classes/Rectangle');
 const Square = require('./classes/Square');
 const Triangle = require('./classes/Triangle');
-
-const circle = new Circle(5);
-console.log(circle.area());
